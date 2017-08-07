d7f843bceb0b1df9b574837e0faf32a83a285bf0
Ilya Nikokoshev <ilya.nikokoshev@gmail.com>
2017-08-07 08:06:52 +0200

    Create the project

---
+++ SMAChallenge/SMAChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SMAChallenge
+//
+//  Created by Ilya Nikokoshev on 7.08.17.
+//  Copyright © 2017 Ilya Nikokoshev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SMAChallenge/SMAChallenge/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SMAChallenge
+//
+//  Created by Ilya Nikokoshev on 7.08.17.
+//  Copyright © 2017 Ilya Nikokoshev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
