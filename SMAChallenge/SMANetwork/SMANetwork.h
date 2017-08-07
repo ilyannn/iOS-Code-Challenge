05df17475027f31a92e858b2807b327b812b4fc7
Ilya Nikokoshev <ilya.nikokoshev@gmail.com>
2017-08-07 08:12:38 +0200

    Add the framework

---
+++ SMAChallenge/SMANetwork/SMANetwork.h
@@ -0,0 +1,13 @@
+//
+//  SMANetwork.h
+//  SMANetwork
+//
+//  Created by Ilya Nikokoshev on 7.08.17.
+//  Copyright © 2017 Ilya Nikokoshev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SMANetwork : NSObject
+
+@end
---
+++ SMAChallenge/SMANetwork/SMANetwork.m
@@ -0,0 +1,13 @@
+//
+//  SMANetwork.m
+//  SMANetwork
+//
+//  Created by Ilya Nikokoshev on 7.08.17.
+//  Copyright © 2017 Ilya Nikokoshev. All rights reserved.
+//
+
+#import "SMANetwork.h"
+
+@implementation SMANetwork
+
+@end
